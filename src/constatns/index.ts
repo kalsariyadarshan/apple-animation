8176486c7c36c44c9786e08a679523acdcecf26e
kalsariyadarshan <142231808+kalsariyadarshan@users.noreply.github.com>
2024-10-18 18:13:25 +0530

    Add files via upload

---
+++ src/constatns/index.ts
@@ -0,0 +1,116 @@
+export const NAVIGATION_LINK = [
+  { name: 'Home', path: '/#Home' },
+  { name: 'Speed', path: '#Speed' },
+  { name: 'Meets', path: '#Meets' },
+  { name: 'Security', path: '#Security' },
+  { name: 'Reviews', path: '#Reviews' },
+];
+
+export const SECTION_CONTENT = [
+  {
+    title: 'Design document',
+  },
+  {
+    title: 'IDE Plugin',
+  },
+  {
+    title: 'GitHub App',
+  },
+  {
+    title: 'Dart , Slack',
+  },
+];
+
+export const WHAT_SETS_UP_APART = [
+  {
+    id: 1,
+    img: 'CustomAIModels.png',
+    headding: 'Custom AI models trained and fine tuned for security',
+    description: 'We train AI models as cybersecurity specialists',
+  },
+  {
+    id: 2,
+    img: 'CustomAgentWorkbench.png',
+    headding: 'Custom Agent workbench with existing  workflows',
+    description:
+      'We have template agent functions for security, want something custom we’ve got your back',
+  },
+  {
+    id: 3,
+    img: 'ClearReasoningAudits.png',
+    headding: 'Clear reasoning audits',
+    description: 'Clear and transparent view into a recommendation',
+  },
+  {
+    id: 4,
+    img: 'ContinuousControlMonitoring.png',
+    headding: 'Continuous control monitoring',
+    description:
+      'Our model and agents stay up-to date and inform you of controls that need to be  tweaked with the latest standards so that you don’t have to',
+  },
+  {
+    id: 5,
+    img: 'HitTheGroundRunning.png',
+    headding: 'Hit the ground running',
+    description:
+      'Learns your tools, context and policies to deliver value  from day 1',
+  },
+  {
+    id: 6,
+    img: 'MeetDevelopers.png',
+    headding: 'Meet developers where they are',
+    description:
+      'IDE Plugins, Slackbot, Github app or a conventional browser assistant or Direct API',
+  },
+];
+
+export const UNDER_THE_HOOD_PART = [
+  {
+    id: 1,
+    img: 'custom_securtity.png',
+    headding: 'Custom Security Model',
+    description:
+      "Organized with variants, ready to adapt. It isn't just another icon set with the trivial import from Illustrator.",
+    color: 'rgba(146, 92, 255, 0.5)',
+  },
+  {
+    id: 2,
+    img: 'made_security.png',
+    headding: 'Made for Security',
+    description:
+      'Crafted with love and great attention to detail. 6 stylish looks to help your UI stand out from the crowd.',
+    color: 'rgba(255, 201, 51, 0.5)',
+  },
+  {
+    id: 3,
+    img: 'efficient.png',
+    headding: 'Efficient and reliable',
+    description:
+      'Classic 24px grid, elegant 1.5px stroke weight by default. Compatible with many other icon libraries.',
+    color: 'rgba(67, 239, 159, 0.5)',
+  },
+  {
+    id: 4,
+    img: 'workflow.png',
+    headding: 'Workflows no problem',
+    description:
+      'Easily change stroke weight or corner radius of any icon. Or make changes on all the icons at once, if required.',
+    color: 'rgba(255, 71, 82, 0.5)',
+  },
+  {
+    id: 5,
+    img: 'fastswitching.png',
+    headding: 'Fast switching between styles',
+    description:
+      'Variants allow you to switch between icon styles without using search. Huge time & energy saver.',
+    color: 'rgba(51, 160, 255, 0.5)',
+  },
+  {
+    id: 6,
+    img: 'smart.png',
+    headding: 'Smart & clean search results',
+    description:
+      "Your search won't be overloaded with icons in all 6 styles. And smart tags will make it even more accurate.",
+    color: 'rgba(255, 133, 51, 0.5)',
+  },
+];

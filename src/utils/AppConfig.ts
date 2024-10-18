4b19c6c06ed52fde771bde26ed5fcf9d99c88416
kalsariyadarshan <142231808+kalsariyadarshan@users.noreply.github.com>
2024-10-18 18:17:15 +0530

    add functiions

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'amwater',
+  title: 'amwater',
+  description: 'amwater',
+  locale: 'en',
+};
